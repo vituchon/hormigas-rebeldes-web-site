cb185c89965e6eb36b77a381021b97dfd053e73f
Vituchon <victor@omniasalud.com>
2025-07-13 17:17:03 -0300

    cleanup: removed not working well  animation (it flicker on mobiles)

--- src/app/components/AnimatedMain.tsx
+++ src/app/components/AnimatedMain.tsx
@@ -1,23 +1,8 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation';
-
 export default function AnimatedMain({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  const [animate, setAnimate] = useState(true);
-
-  useEffect(() => {
-    setAnimate(false);
-    const timeout = setTimeout(() => setAnimate(true), 0);
-    return () => clearTimeout(timeout);
-  }, [pathname]);
-
   return (
-    <main
-      className={`max-w-5xl mx-auto px-4 py-12 ${animate ? 'fade-in' : ''}`}
-      style={{ visibility: animate ? 'visible' : 'hidden' }}
-    >
+    <main className={`max-w-5xl mx-auto px-4 py-12 `}>
       {children}
     </main>
   );
